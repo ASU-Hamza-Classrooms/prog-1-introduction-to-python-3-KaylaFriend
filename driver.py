67106e1e71d072599dd5eefbaa0e5651ae227135
Kayla Friend <friendka@appstate.edu>
2022-09-12 14:29:24 -0400

    Completed programming assignment

--- driver.py
+++ driver.py
@@ -9,10 +9,13 @@ import sys
 #Function to test the functions in the stringlib module
 def testStringLib(inputStr):
     #Add code to call each of the functions and print the results
-    print(reverseStr(inputStr))
-    print(containsWord(inputStr, "banana"))
-    print(isPalindrome(inputStr))
-    print(upperCaseStr(inputStr))
+    print("Input string is %s" % (inputStr))
+    print("Reverse of string is %s" % (reverseStr(inputStr)))
+    print("Does string contain IT? %s" % containsWord(inputStr, "IT"))
+    print("Does string contain apple? %s" % containsWord(inputStr, "apple"))
+    print("Does string contain data? %s" % containsWord(inputStr, "data"))
+    print("Is string a palindrome? %s" % isPalindrome(inputStr))
+    print("Uppercase of string is %s" % upperCaseStr(inputStr))
     return
 
 #Function to test the methods in the Worker class in the worker module
@@ -20,6 +23,15 @@ def testWorkerClass(inputStr):
     #Add code to create a Worker object
     #Use the object to call each of the methods in the Worker class
     #Print the result of each call
+    obj = worker(inputStr)
+    print("Input string is %s" % (inputStr))
+    print("Reverse of string is %s" % obj.reverse())
+    print("Does string contain IT? %s" % obj.containsStr("IT"))
+    print("Does string contain apple? %s" % obj.containsStr("apple"))
+    print("Does string contain data? %s" % obj.containsStr("data"))
+    print("Is string a palindrome? %s" % obj.palindrome())
+    print("Uppercase of string is %s" % obj.upperCase())
+
     return
 
 #check to make sure there is a string command line argument
--- worker.py
+++ worker.py
@@ -15,3 +15,33 @@ from stringlib import *
 #Your method can call the needed function in the stringlib
 #module.
 
+class worker:
+    def __init__(self, input):
+        self.str = input
+    
+    #reverseStr - takes as input a string and returns the reverse of it
+    def reverse(self):
+        return self.str[::-1]
+
+    #containsWord - takes as input two strings, containingStr and containedStr,
+    #and returns "Yes" if containedStr is anywhere within containingStr 
+    #and returns "No" otherwise
+    def containsStr(self, containedStr):
+        if self.str != None and containedStr.lower() in self.str.lower():
+            return "Yes"
+        else:
+            return "No"
+
+    #isPalindrome - takes as input a string and returns "Yes" if it is
+    #palindrome and "No" otherwise  
+    def palindrome(self):
+        if reverseStr(self.str) == self.str: 
+            return "Yes"
+        else : 
+            return "No"
+
+    #upperCaseStr - takes as input a string and returns the identical string
+    #with the characters 'a' ... 'z' changed to uppercase
+    def upperCase(self):
+        return self.str.upper()
+
